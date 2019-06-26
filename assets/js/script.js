ba316c89a94b6168db74af9ab793b6a1b25c6949
Shengming Yuan <syua9708@flhs.us>
2019-06-25 23:41:43 -0400

    include video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 //CSS framework init
 document.addEventListener('DOMContentLoaded', function() {
     M.FormSelect.init(document.querySelectorAll('select'));
+    var players = Plyr.setup('.video',{
+        controls:['play-large', 'play', 'progress', 'current-time', 'mute', 'volume','pip', 'airplay', 'fullscreen']
+    });
     M.Sidenav.init(document.getElementsByClassName('sidenav')[0]);
+    var modalInstances = M.Modal.init(document.getElementById('promoVideo'),{
+        onCloseStart: () => {
+            players.map(p => {
+                p.stop();
+            });
+        }
+    });
+    playPromoVideo(modalInstances);
 });
+function playPromoVideo(modalInstances) {
+    var val = localStorage.getItem('1st_visit');
+    if (!val) {
+        modalInstances.open();
+        localStorage.setItem('1st_visit', 'false');
+    }
+}
